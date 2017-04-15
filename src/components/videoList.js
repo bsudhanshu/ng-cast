cf9233b20489bc9d014328cf4e1645dd9771eec1
Hack Reactor Student <student@hackreactor.com>
2017-04-14 21:05:01 -0700

    Upto videolist test cases

--- src/components/videoList.js
+++ src/components/videoList.js
@@ -1,6 +1,19 @@
 angular.module('video-player')
+
 .directive('videoList', function() {
+
   return {
     // TODO
+    scope: {
+    	videos: '<',
+    	onClick: '<'
+    },
+    controller: function () {
+      this.videos= 'exampleVideoData';
+    },
+    controllerAs: 'ctrl',
+    videos: {{ctrl}}.exampleVideoData,
+    bindToController: true,
+    template: 'src/templates/videoList.html',    
   };
 });
